347b5c374e703e651779ec33121e470c1cb280f6
zidanreza <161805545+zidanreza@users.noreply.github.com>
2024-04-25 18:14:36 +0700

    Update Character.java

--- Character.java
+++ Character.java
@@ -19,35 +19,33 @@ public abstract class Character {
         this.currentMP = this.maxMP;
     }
 
-    public abstract void attack(Character enemy); //menyerang 
+    public abstract void attack(Character enemy);
 
-    public void usePotion(int amount) { //ramuan
+    public void usePotion(int amount) { 
         this.currentHP = Math.min(this.maxHP, this.currentHP + amount);
-    } // Metode untuk menggunakan item Potion yang akan menambah currentHP sejumlah amount (tetapi tidak melebihi maxHP).
+    } 
 
     public void useEther(int amount) {
         this.currentMP = Math.min(this.maxMP, this.currentMP + amount);
-    } //useEther(int jumlah): Metode untuk menggunakan item Ether yang akan menambah currentMP sejumlah jumlah (tetapi tidak melebihi maxMP).
+    } 
 
     public void useElixir(int hpAmount, int mpAmount) {
         usePotion(hpAmount);
         useEther(mpAmount);
-    } //useElixir(int hpAmount, int mpAmount): Metode untuk menggunakan item Elixir yang akan menambah currentHP sejumlah hpAmount dan currentMP sejumlah mpAmount.
+    } 
 
     private int hitungMaxHP(int level) {
-        // Implementasi perhitungan maxHP berdasarkan level
-        return level * 50; // Contoh: HP maksimum adalah 50 per level
+        return level * 50;
     }
 
     private int hitungMaxMP(int level) {
-        // Implementasi perhitungan maxMP berdasarkan level
-        return level * 50; // Contoh: MP maksimum adalah 50 per level
+        return level * 50;
     }
 
-    public void receiveDamage(int damage) { // menerima damage 
+    public void receiveDamage(int damage) {
         int effectiveDamage = Math.max(0, damage - (armor != null ? armor.getDefensePoints() : 0));
         this.currentHP = Math.max(0, this.currentHP - effectiveDamage);
 
-    } // receiveDamage(int damage): Metode untuk mengurangi currentHP karakter berdasarkan damage yang diterima setelah dipertimbangkan dengan defensePoints dari armor.
+    }
     
 }
