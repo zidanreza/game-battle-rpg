458c43f80722304779cc8cf4c90ba0feaaf62e3a
zidanreza <161805545+zidanreza@users.noreply.github.com>
2024-04-25 18:02:40 +0700

    Create Weapon.java

---
+++ Weapon.java
@@ -0,0 +1,17 @@
+public class Weapon {
+    private String type;
+    private int attackPoints;
+
+    public Weapon(String type, int attackPoints) {
+        this.type = type;
+        this.attackPoints = attackPoints;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getAttackPoints() {
+        return attackPoints; // titik serang
+    }
+}
